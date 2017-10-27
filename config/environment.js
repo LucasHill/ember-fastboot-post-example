88aff9e408b19da9e7298bf5b42d003886c5a91c
Lucas Hill <kingofallhills@gmail.com>
2017-10-26 21:59:21 -0700

    Create in repo addon/middleware to support posts.

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,9 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    
+    historySupportMiddleware: false,
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
