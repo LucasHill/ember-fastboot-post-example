20310f33c3cd15c400a72b450e2c6609ddaede7b
Lucas Hill <kingofallhills@gmail.com>
2017-10-26 22:39:37 -0700

    Add body parser middleware and example.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,4 +5,8 @@ export default Controller.extend({
   fastboot: Ember.inject.service(),
 
   isPost: Ember.computed.equal('fastboot.request.method', 'POST'),
+
+  postMessage: Ember.computed('fastboot.request.body', function() {    
+    return this.get('fastboot.request.body.message');
+  })
 });
